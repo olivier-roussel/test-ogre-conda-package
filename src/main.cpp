4d466004f94597ce3e837a149dbc5e91091668a6
Olivier Roussel <olivier.roussel@inria.fr>
2025-11-20 16:15:57 +0100

    reducing to RME

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-#include <vpConfig.h>
+#include <Ogre.h>
+#include <OgreFrameListener.h>
+
+#include <OgreRectangle2D.h>
 
 int main(void)
 {
-    std::cout << "VISP_HAVE_OGRE_VERSION = " << VISP_HAVE_OGRE_VERSION << std::endl;
-    const unsigned int ogre_version_api_change = (1<<16 | 11 <<8 | 0);
-    std::cout << "(1<<16 | 11 <<8 | 0) = " << ogre_version_api_change << std::endl;
 
     return 0;
 }
